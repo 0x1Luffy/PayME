e5c4d62695738d10e5858205a02c3f7a085dba07
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2024-03-25 11:40:37 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,50 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/Signin";
-import { Dashboard } from "./pages/Dashboard";
-import { SendMoney } from "./pages/SendMoney";
-import { useEffect } from "react";
-
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Home from './pages/Home';
 
 function App() {
-  const token = localStorage.getItem('token');
-  useEffect(() => {
-    if (token && window.location.pathname !== '/dashboard') {
-
-      window.location.href = '/dashboard';
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(()=>{
+    async function fetch(){
+      const response= await axios.get("http://localhost:3000/api/v1/me",{
+      headers:{
+        Authorization:"Bearer "+ localStorage.getItem("token")
+      }
+    })
+    if(response.data.authentication){
+      setIsAuthenticated(true);
     }
-  }, []);
+    else {
+      setIsAuthenticated(false);
+    }
+    }
+    fetch(); 
+  },[])
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<SendMoney />} />
-        <Route path="*" element={<Navigate to="/signin" />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        {isAuthenticated ? (
+          <>
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/send' element={<SendMoney />} />
+            <Route path='*' element={<Navigate to="/dashboard" />} />
+          </>
+        ) : (
+          <>
+            <Route path='/dashboard' element={<Navigate to="/signin" />} />
+            <Route path='/send' element={<Navigate to="/signin" />} />
+            
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
