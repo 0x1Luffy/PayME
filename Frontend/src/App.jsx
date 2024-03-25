485738465ab2048bdd01e52fd76c55da7a4d0ace
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2024-03-25 11:34:02 +0530

    Testing !!!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Dashboard from "./pages/Dashboard";
-import SendMoney from "./pages/SendMoney";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Home from './pages/Home';
-import { baseURL } from "../URLs";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import { useEffect } from "react";
+
 
 function App() {
   const token = localStorage.getItem('token');
@@ -16,25 +19,15 @@ function App() {
       window.location.href = '/dashboard';
     }
   }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/signin' element={<Signin />} />
-        {isAuthenticated ? (
-          <>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/send' element={<SendMoney />} />
-            <Route path='*' element={<Navigate to="/dashboard" />} />
-          </>
-        ) : (
-          <>
-            <Route path='/dashboard' element={<Navigate to="/signin" />} />
-            <Route path='/send' element={<Navigate to="/signin" />} />
-            
-          </>
-        )}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+        <Route path="*" element={<Navigate to="/signin" />} />
       </Routes>
     </BrowserRouter>
   );
