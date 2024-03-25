f93277db16bbbd19f511c04601c5c6c32e563729
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2024-03-25 12:28:14 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,35 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import  { useEffect } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import SendMoney from "./pages/SendMoney";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  useEffect(()=>{
-    async function fetch(){
-      const response= await axios.get("http://localhost:3000/api/v1/me",{
-      headers:{
-        Authorization:"Bearer "+ localStorage.getItem("token")
-      }
-    })
-    if(response.data.authentication){
-      setIsAuthenticated(true);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (token && window.location.pathname !== '/dashboard') {
+      
+      return <Navigate to="/dashboard" />;
     }
-    else {
-      setIsAuthenticated(false);
-    }
-    }
-    fetch(); 
-  },[])
+  }, [token]);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/signin' element={<Signin />} />
-        {isAuthenticated ? (
-          <>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/send' element={<SendMoney />} />
-            <Route path='*' element={<Navigate to="/dashboard" />} />
-          </>
-        ) : (
-          <>
-            <Route path='/dashboard' element={<Navigate to="/signin" />} />
-            <Route path='/send' element={<Navigate to="/signin" />} />
-            
-          </>
-        )}
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+        <Route path="*" element={<Navigate to="/signin" />} />
       </Routes>
     </BrowserRouter>
   );
